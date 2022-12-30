3e05b137d51859c1e010d27e1f2e9407eb671cb8
CarlosAFK <carlos.escobedo@alumnos.uv.cl>
2022-12-30 00:15:31 -0300

    Cambio de estilos home

--- src/views/home.php
+++ src/views/home.php
@@ -7,13 +7,27 @@ $crud = new Note();
 $datos = $crud->get();
 ?>
 <h1> Home</h1>
+<div style="  display: flex;
+flex-wrap: wrap;
+gap: 5px;
+padding: 10px;">
 <?php foreach ($datos as $nota) {
 ?>
-    <div style="padding-top: 20px;">
-        <h5>Nota: <?php echo $nota->title; ?></h5>
+    <div style="background-color: white;
+  width: 400px;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  border: solid 2px transparent;
+  border-radius: 5px;">
+
+        <h5><?php echo $nota->title; ?></h5>
         <a class="btn btn-primary" href="?view=view&id=<?php echo $nota->_id; ?>">
             Ir <i class="fas fa-arrow-right-from-bracket"></i>
         </a>
     </div>
-<?php } ?>
+    <?php } ?>
+</div>
 <?php require_once 'src/views/resources/scripts.php'; ?>
